fc19d4cc9b9104cf3f66e7e6dfe9a991c6392b4d
Elmanov_Evgeniy <elmanov21@gmail.com>
2023-01-16 17:18:57 +0800

    Add example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,82 @@
+﻿// void FillArray(int[] collechion)
+// {
+//   int Length = collechion.Length;
+//   int index = 0;
+//   while (index< Length)
+//   {
+//     collechion[index] = new Random().Next(1, 10);
+//     index++;
+//   }
+// }
+
+// void PrintArray(int[] col)
+// {
+//   int count = col.Length;
+//   int position = 0;
+//   while (position < count)
+//   {
+//     Console.WriteLine(col[position]);
+//     position++;
+//   }
+
+// }
+
+
+// int[] array = new int[10];
+
+// FillArray(array);
+// PrintArray(array);
+
+
+void FillArray(int[] collechion)
+{
+  int Length = collechion.Length;
+  int index = 0;
+  while (index< Length)
+  {
+    collechion[index] = new Random().Next(1, 10);
+    index++;
+  }
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  while (position < count)
+  {
+    Console.WriteLine(col[position]);
+    position++;
+  }
+
+}
+
+int Indexof(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = 0;
+
+  while (index < count)
+  {
+    if(collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6]= 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
